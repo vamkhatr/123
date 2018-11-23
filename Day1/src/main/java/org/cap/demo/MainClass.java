de413fb5522c14f2df95c5635abfc22190cdadf5
vamkhatr <vamkhatr@DIN73003055.corp.capgemini.com>
2018-11-23 14:31:19 +0530

    First Commit

---
+++ Day1/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! GAFNN");
+		greetings();
+
+	}
+	
+	public static void greetings() {
+		System.out.println("Welcome to git");
+	}
+
+}
